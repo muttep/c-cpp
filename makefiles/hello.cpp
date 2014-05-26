99db414828ce5ad83874571bdc8a6f6290ad9726
Pandu <pandu.ranganadh@gmail.com>
2014-05-26 17:07:13 +0530

    adding sample code + makefiles

---
+++ makefiles/hello.cpp
@@ -0,0 +1,8 @@
+//#include <iostream.h>
+#include <stdio.h>
+#include "functions.h"
+
+void print_hello(){
+//   cout << "Hello World!";
+	printf ("Hello World!");
+}
---
+++ makefiles/main.cpp
@@ -0,0 +1,12 @@
+//#include <iostream.h>
+#include <stdio.h>
+#include "functions.h"
+
+int main(){
+    print_hello();
+//    cout << endl;
+//    cout << "The factorial of 5 is " << factorial(5) << endl;
+    printf("\nThe factorial of 5 is : %d\n", factorial(5));
+    printf ("\n");
+    return 0;
+}
